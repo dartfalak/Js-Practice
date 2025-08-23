e47035a8aee338aa7cd6287ddd9c1e1cb1d9c5aa
dartfalak <falakcles@gmail.com>
2025-08-23 23:28:26 +0500

    add loop examples

--- first.js
+++ first.js
@@ -54,8 +54,13 @@ for (let i=1; i <=10; i++) {
   
 console.log ("loop has ended");
 
-let i=1;
-do {
-    console.log ("i = ", i)
-    i++;
-} while (i <=10)
+// let i=1;
+// do {
+//     console.log ("i = ", i)
+//     i++;
+// } while (i <=10)
+
+let str="Hello"
+for (let i of str) {
+    console.log("i=",i);
+}
