79d6e98a37302d4d1e1041cd77bc4b5a23116888
dartfalak <falakcles@gmail.com>
2025-09-17 23:44:31 +0500

    add dark/light mode toggle button

--- first.js
+++ first.js
@@ -359,8 +359,10 @@ let currentMode = "light";
 modeBtn.addEventListener("click", () => {
   if(currentMode === "light") {
     currentMode = "dark";
+    document.querySelector("body").style.backgroundColor = "black";
   } else {
     currentMode = "light";
+    document.querySelector("body").style.backgroundColor = "white";
   }
   console.log("Current mode:", currentMode);
 });
