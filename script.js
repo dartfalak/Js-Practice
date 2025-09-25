99d6ef9583511ac89cf3c697ec8dd6859a5e1e39
dartfalak <falakcles@gmail.com>
2025-09-25 22:33:43 +0500

    practice: promises

--- script.js
+++ script.js
@@ -37,123 +37,123 @@
 //     fadeText.style.opacity = "1";
 // });
 
-const user1 = {
-    name: "John",
-    role: "admin",
-    isActive: true
-};
-
-// const user2 = {
-//     name: "Alice",
+// const user1 = {
+//     name: "John",
 //     role: "admin",
 //     isActive: true
 // };
 
-// user1.__proto__ = user2;
+// // const user2 = {
+// //     name: "Alice",
+// //     role: "admin",
+// //     isActive: true
+// // };
 
-class User {
-    constructor(id, name, isActive, role=
-         "User")  {
-        this.id = id;
-        this.name = name;
-        this.isActive = isActive;
-        this.role = role;
-
-
-    }
-};
-
-const user3 = new User (1, "Bob", true); 
-
-class Admin extends User {}
-
-let admin1 = new Admin (2, "Charlie", true, "Admin");
-
-class Person {
-    constructor() {
-        this.eat = "Eating...";
-}
-    sleep() {
-        console.log("Sleeping...");
-    }
-};
-
-class Developer extends Person {
-    constructor(branch) {
-        super();
-        this.branch = branch;
-    }
-}
-
-let developerObj = new Developer("frontend");
-
-
-// let info = "Developer Branch"
+// // user1.__proto__ = user2;
 
 // class User {
-//     constructor(name, age) {
+//     constructor(id, name, isActive, role=
+//          "User")  {
+//         this.id = id;
 //         this.name = name;
-//         this.age = age;
-// }
+//         this.isActive = isActive;
+//         this.role = role;
 
-// viewDetails() {
-//     console.log(`Name: ${this.name}, Age: ${this.age}`);
+
+//     }
+// };
+
+// const user3 = new User (1, "Bob", true); 
+
+// class Admin extends User {}
+
+// let admin1 = new Admin (2, "Charlie", true, "Admin");
+
+// class Person {
+//     constructor() {
+//         this.eat = "Eating...";
+// }
+//     sleep() {
+//         console.log("Sleeping...");
+//     }
+// };
+
+// class Developer extends Person {
+//     constructor(branch) {
+//         super();
+//         this.branch = branch;
 //     }
 // }
 
-// class Member extends User {
-//     constructor(name, age) {
-//         super(name, age);
+// let developerObj = new Developer("frontend");
+
+
+// // let info = "Developer Branch"
+
+// // class User {
+// //     constructor(name, age) {
+// //         this.name = name;
+// //         this.age = age;
+// // }
+
+// // viewDetails() {
+// //     console.log(`Name: ${this.name}, Age: ${this.age}`);
+// //     }
+// // }
+
+// // class Member extends User {
+// //     constructor(name, age) {
+// //         super(name, age);
     
+// //     }
+// //     editDetails() {
+// //             info = "Member Branch";
+// //   }
+// // }
+
+
+
+// // let user2 = new User("David", 30);
+
+
+// // class User {
+// //     constructor(name) {
+// //         this.name = name;
+// //     }
+
+// //     sayHello() {
+// //         console.log(`Hello, ${this.name}`);
+// //   }
+// // }
+
+// // class Member extends User {
+// //     constructor(name, memberId) {
+// //         super(name);
+// //         this.memberId = memberId;
+// //     }
+
+// //     displayInfo() {
+// //         console.log(`Name: ${this.name}, Member ID: ${this.memberId}`);
+// //     }
+// // }
+
+// // let member1 = new Member("Eve", 101);
+
+// function getInput (inputId,getNextId) {
+//     setTimeout(() => {
+//     console.log("inputId:", inputId)
+//     if(getNextId) {
+//         getNextId();
 //     }
-//     editDetails() {
-//             info = "Member Branch";
-//   }
+//  }, 1000);
+
+
 // }
 
+// getInput(1,() => {
+//     getInput(2);
+// });
 
-
-// let user2 = new User("David", 30);
-
-
-// class User {
-//     constructor(name) {
-//         this.name = name;
-//     }
-
-//     sayHello() {
-//         console.log(`Hello, ${this.name}`);
-//   }
-// }
-
-// class Member extends User {
-//     constructor(name, memberId) {
-//         super(name);
-//         this.memberId = memberId;
-//     }
-
-//     displayInfo() {
-//         console.log(`Name: ${this.name}, Member ID: ${this.memberId}`);
-//     }
-// }
-
-// let member1 = new Member("Eve", 101);
-
-function getInput (inputId,getNextId) {
-    setTimeout(() => {
-    console.log("inputId:", inputId)
-    if(getNextId) {
-        getNextId();
-    }
- }, 1000);
-
-
-}
-
-getInput(1,() => {
-    getInput(2);
+let promise = new Promise((resolve, reject) => {
+    console.log("Promise started");
 });
-
-// getInput(2);
-// getInput(3);
-
