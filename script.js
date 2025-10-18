6698cfa7ffb846ed077e52debaf82fae651b8f50
dartfalak <falakcles@gmail.com>
2025-10-18 23:18:31 +0500

    practice: if/else

--- script.js
+++ script.js
@@ -548,3 +548,16 @@ showTextBtn.addEventListener("click", function() {
     }
 });
 
+let score = 72;
+
+if (score >= 90) {
+  console.log("Grade: A");
+} else if (score >= 80) {
+  console.log("Grade: B");
+} else if (score >= 70) {
+  console.log("Grade: C");
+} else if (score >= 60) {
+  console.log("Grade: D");
+} else {
+  console.log("Grade: F");
+}
