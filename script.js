09444b382d7cd85a35b2dd3d8590b31caa0038eb
dartfalak <falakcles@gmail.com>
2025-10-18 23:21:01 +0500

    practice: if/else

--- script.js
+++ script.js
@@ -561,3 +561,12 @@ if (score >= 90) {
 } else {
   console.log("Grade: F");
 }
+
+
+let number = 7;
+
+if (number % 2 === 0) {
+  console.log(number + " is even");
+} else {
+  console.log(number + " is odd");
+}
