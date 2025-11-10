afd0f0f25e24321c0a1fa20040c1734d9e3fd1f8
dartfalak <falakcles@gmail.com>
2025-11-10 22:35:13 +0500

    practice: spread/rest operators

--- script.js
+++ script.js
@@ -1273,3 +1273,13 @@ let numbers = [1, 2, 3, 4, 5];
 let maximum = Math.max(...numbers);
 let minimum = Math.min(...numbers);
 console.log("Max:", maximum);
+
+function arrconcat(...arrays) {
+  console.log([].concat(...arrays));
+}
+
+
+
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+const arr3 = [7, 8, 9];
