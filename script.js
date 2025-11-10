25800db9ea1d404742caff150417118bb5e5c166
dartfalak <falakcles@gmail.com>
2025-11-10 22:35:27 +0500

    practice: spread/rest operators

--- script.js
+++ script.js
@@ -1278,8 +1278,8 @@ function arrconcat(...arrays) {
   console.log([].concat(...arrays));
 }
 
-
-
 const arr1 = [1, 2, 3];
 const arr2 = [4, 5, 6];
 const arr3 = [7, 8, 9];
+
+arrconcat(arr1, arr2, arr3);
