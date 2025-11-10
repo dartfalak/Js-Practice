ad79988873db83f1b6a34ff586657156cfe119a2
dartfalak <falakcles@gmail.com>
2025-11-10 22:31:41 +0500

    practice: spread/rest operators

--- script.js
+++ script.js
@@ -1271,4 +1271,5 @@ console.log(salary);
 
 let numbers = [1, 2, 3, 4, 5];
 let maximum = Math.max(...numbers);
-console.log(...numbers);
+let minimum = Math.min(...numbers);
+console.log("Max:", maximum);
