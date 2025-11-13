b47f1718b4a765fa85af6df0c4fa6d10ca689730
dartfalak <falakcles@gmail.com>
2025-11-13 23:38:47 +0500

    practice: async await

--- script.js
+++ script.js
@@ -1269,20 +1269,20 @@
 // console.log(salary);       
 
 
-let numbers = [1, 2, 3, 4, 5];
-let maximum = Math.max(...numbers);
-let minimum = Math.min(...numbers);
-console.log("Max:", maximum);
+// let numbers = [1, 2, 3, 4, 5];
+// let maximum = Math.max(...numbers);
+// let minimum = Math.min(...numbers);
+// console.log("Max:", maximum);
 
-function arrconcat(...arrays) {
-  console.log([].concat(...arrays));
-}
+// function arrconcat(...arrays) {
+//   console.log([].concat(...arrays));
+// }
 
-const arr1 = [1, 2, 3];
-const arr2 = [4, 5, 6];
-const arr3 = [7, 8, 9];
+// const arr1 = [1, 2, 3];
+// const arr2 = [4, 5, 6];
+// const arr3 = [7, 8, 9];
 
-arrconcat(arr1, arr2, arr3);
+// arrconcat(arr1, arr2, arr3);
 
 
 async function greet() {
