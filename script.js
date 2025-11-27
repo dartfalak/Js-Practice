f6716e9b1152d961d00ea7059ca6ff10467c7efe
dartfalak <falakcles@gmail.com>
2025-11-27 23:42:16 +0500

    practice: async await

--- script.js
+++ script.js
@@ -1561,3 +1561,9 @@ async function process() {
 
 process();
 
+function addAfterDelay(a, b, delay) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(a + b);
+        }, delay);
+    });
