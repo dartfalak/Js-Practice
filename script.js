e7ef84ac7833ce71c1b7d587099292d1044c689a
dartfalak <falakcles@gmail.com>
2025-12-13 21:54:06 +0500

    add calculator IIFE

--- script.js
+++ script.js
@@ -1707,3 +1707,4 @@ const calculator = (function() {
 console.log(calculator.add(2, 6));
 console.log(calculator.subtract(10, 4));
 console.log(calculator.multiply(3, 5));
+console.log(calculator.divide(20, 4));
