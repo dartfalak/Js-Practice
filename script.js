f1712eac0e03828eb7f8b6f2819a7ab41dfb0fce
dartfalak <falakcles@gmail.com>
2026-01-25 17:59:12 +0500

    practice: function chaining

--- script.js
+++ script.js
@@ -2336,3 +2336,8 @@ const evenNumbers = [2, 4, 6, 8, 10]
     .map(num => num + 1);
 console.log(evenNumbers);
 
+const oddNumbers = [1, 3, 5, 7, 9]
+    .filter(num => num % 3 === 0)
+    .map(num => num * 2);
+console.log(oddNumbers);
+
