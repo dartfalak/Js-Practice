e3388233ef0599983b8cc38ac615d3137a961db1
dartfalak <falakcles@gmail.com>
2026-01-25 18:00:28 +0500

    practice: function chaining

--- script.js
+++ script.js
@@ -2347,3 +2347,7 @@ const squaredSum = [1, 2, 3, 4, 5]
 console.log(squaredSum);
 
 const calculateNumbers = [10, 15, 20, 25, 30]
+    .filter(num => num > 15)
+    .map(num => num / 5)
+    .reduce((acc, num) => acc + num, 0);
+console.log(calculateNumbers);
