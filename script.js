6da388babfde475419c28407d85ec1c7e76f955e
dartfalak <falakcles@gmail.com>
2026-01-27 22:57:59 +0500

    practice: function chaining

--- script.js
+++ script.js
@@ -2370,3 +2370,11 @@ const resultNames = names
     .join(", ");
 
 console.log(resultNames);
+
+
+const numbers = [1, 2, 3, 4, 5, 6];
+const processed = numbers
+    .filter(n => n % 2 === 0)
+    .map(n => n * n)
+    .reduce((sum, n) => sum + n, 0);
+console.log(processed);
