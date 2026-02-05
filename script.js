4f1858f49a3a08b2a2b7407dfa34534b2584f7a9
dartfalak <falakcles@gmail.com>
2026-02-05 23:37:07 +0500

    practice: function chaining

--- script.js
+++ script.js
@@ -2453,4 +2453,6 @@ const numbers = [1,2,3,4,5]
 
 const processedNumbers = numbers 
    .map(n => n + 1)
-    .filter(n => n % 2 === 0)
+   .filter(n => n % 2 === 0);
+
+console.log(processedNumbers);
